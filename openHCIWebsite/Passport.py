1b2bc3db94d7367a54d220f70bd1bcd6699a6f9b
sodas <sodas2002@gmail.com>
2011-07-28 10:41:30 +0800

    Add new example

---
+++ openHCIWebsite/Passport.py
@@ -0,0 +1,24 @@
+import os
+
+from google.appengine.dist import use_library
+use_library("django", "1.2")
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from Constants import debugMode
+
+class PassportPage(webapp.RequestHandler):
+    def get(self):
+        pagePath = os.path.join(os.path.dirname(__file__), "html/passport.html")
+        pageDict = {}
+        renderResult = template.render(pagePath, pageDict)
+        self.response.out.write(renderResult)
+
+application = webapp.WSGIApplication([("/passport/", PassportPage)], debug=debugMode)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
---
+++ openHCIWebsite/Root.py
@@ -0,0 +1,31 @@
+import os
+from random import randint
+
+from google.appengine.dist import use_library
+use_library("django", "1.2")
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from Constants import debugMode
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        if self.request.get("m")!="false":
+            userAgent = self.request.headers["User-Agent"]
+            if "iPad" in userAgent or "iPhone" in userAgent or "iPod" in userAgent or "Android" in userAgent:
+                self.redirect("/m/")
+                return
+        
+        pageDict = {"randomNumber": randint(0,6)}
+        pagePath = os.path.join(os.path.dirname(__file__), "html/index.html")
+        renderResult = template.render(pagePath, pageDict)
+        self.response.out.write(renderResult)
+
+application = webapp.WSGIApplication([("/", MainPage)], debug=debugMode)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
